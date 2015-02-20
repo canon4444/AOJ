571580eb74aa9b64fc4bab2d76529adacb6ea2b5
canon4444 <haruharu724@hotmail.com>
2015-02-20 11:07:24 +0900

    Create ITP1_2_A_Small,-Large,-or-Equal.c

---
+++ Introduction/Introduction-to-Programming/ITP1_2_A_Small,-Large,-or-Equal.c
@@ -0,0 +1,15 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_2_A
+
+#include <stdio.h>
+ 
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+ 
+    if( a < b ) printf("a < b\n");
+    else if( a > b ) printf("a > b\n");
+    else printf("a == b\n");
+ 
+    return 0;
+}
