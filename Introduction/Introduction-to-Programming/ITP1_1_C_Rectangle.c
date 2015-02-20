ceaa4692ad8043e00f4f29b6727275dc6707cd7f
canon4444 <haruharu724@hotmail.com>
2015-02-20 11:04:14 +0900

    Create ITP1_1_C_Rectangle.c

---
+++ Introduction/Introduction-to-Programming/ITP1_1_C_Rectangle.c
@@ -0,0 +1,13 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_C
+
+#include <stdio.h>
+ 
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+ 
+    printf("%d %d\n", a * b, 2 * a + 2 * b);
+ 
+    return 0;
+}
