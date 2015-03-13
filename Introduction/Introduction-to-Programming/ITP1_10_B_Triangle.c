9da999c682613af2ed0b3eb727bc3701b467a83a
canon4444 <haruharu724@hotmail.com>
2015-03-13 19:13:32 +0900

    Create ITP1_10_B_Triangle.c

---
+++ Introduction/Introduction-to-Programming/ITP1_10_B_Triangle.c
@@ -0,0 +1,24 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_10_B
+
+#include <stdio.h>
+#include <math.h>
+
+double toRadian(int degree)
+{
+    return (degree / (180 / M_PI));
+}
+
+int main()
+{
+    int a, b, C;
+    scanf("%d%d%d", &a, &b, &C);
+
+    double S, L, h;
+    S = (double)(a * b * sin(toRadian(C)) / 2);
+    double c = sqrt(pow(a, 2) - 2*a*b*cos(toRadian(C)) + pow(b, 2));
+    L = (double)(a + b + c);
+    h = (double)(2 * S / a);
+    printf("%lf\n%lf\n%lf\n", S, L, h);
+
+    return 0;
+}
