2043767d8f685cda74eb8a51137e214b0a924a6e
canon4444 <canon4444@users.noreply.github.com>
2015-09-26 20:01:53 +0900

    Create ALDS1_10_A_Fibonacci_Number.c

---
+++ Introduction/Introduction-to-Algorithms-and-Data-Structures/ALDS1_10_A_Fibonacci_Number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int dp[45];
+
+int fib(int n)
+{
+    if( n == 0 || n == 1 ){
+        dp[n] = 1;
+    } else if( dp[n] == -1 ){ //この条件がないとTLE
+        dp[n] = fib(n-1) + fib(n-2);
+    }
+
+    return dp[n];
+}
+
+int main()
+{
+    int i;
+    for( i = 0; i < 45; ++i ) dp[i] = -1;
+
+    int n;
+    scanf("%d", &n);
+
+    printf("%d\n", fib(n));
+
+    return 0;
+}
